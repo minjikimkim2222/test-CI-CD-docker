040500ba1e5d2fb6ead59cc47562744c6a192392
minjikimkim2222 <olivia0929@naver.com>
2024-12-22 19:28:22 +0900

    test cicd code edited

--- src/main/java/test/testcicddocker/TestController.java
+++ src/main/java/test/testcicddocker/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello CICD with Docker! 12/22 ";
+        return "Hello CICD with Docker! 12/22 ! ";
     }
 
 }
